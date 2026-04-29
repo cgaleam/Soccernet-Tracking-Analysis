d2d2698852a9cbf0e219aa72c52204a70b9d9c6c
cgaleam <carlos28galea@gmail.com>
2026-04-29 16:00:56 +0200

    Reorganizar results para evaluacion con metricas

--- src/utils/reorganize_reults.py
+++ src/utils/reorganize_reults.py
@@ -2,16 +2,14 @@ import os
 import shutil
 
 for tracker in ['bytetracker', 'ocsort']:
-    tracker_dir = f'results/{tracker}'
-    for filename in os.listdir(tracker_dir):
-        if filename.endswith('.txt'):
-            seq_name = filename.replace('.txt', '')
-            seq_dir = os.path.join(tracker_dir, seq_name)
-            os.makedirs(seq_dir, exist_ok=True)
-            shutil.move(
-                os.path.join(tracker_dir, filename),
-                os.path.join(seq_dir, filename)
-            )
-            print(f'Movido: {tracker}/{filename} → {tracker}/{seq_name}/{filename}')
+    tracker_dir = os.path.join('results', tracker)
+    for seq_name in os.listdir(tracker_dir):
+        seq_dir = os.path.join(tracker_dir, seq_name)
+        if os.path.isdir(seq_dir):
+            txt_file = os.path.join(seq_dir, seq_name + '.txt')
+            if os.path.exists(txt_file):
+                shutil.move(txt_file, os.path.join(tracker_dir, seq_name + '.txt'))
+                os.rmdir(seq_dir)
+                print(f'Movido: {tracker}/{seq_name}.txt')
 
-print('Reorganización completada.')
+print('Listo.')
